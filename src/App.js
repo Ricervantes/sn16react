4746bf6e75013501061674b7e2d61f5ec40ca3ef
Ricervantes <racervantes.dwfsc9@ucamp.utel.edu.mx>
2022-10-22 10:00:40 -0500

    firebase

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+    </>
+  );
+}
+
+export default App;
